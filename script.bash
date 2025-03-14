3cbbbc670b6058a418b7f89fa96ded7789d00f79
Pawel Galkowski <poczta@itgalkowski.pl>
2025-03-14 22:13:07 +0100

    update files

---
+++ script.bash
@@ -0,0 +1,21 @@
+#!/bin/bash
+DAY=$(date +'%d')
+cd files/
+rm *
+FILE_NAME="new-file$($DAY).js"
+touch $FILE_NAME
+cat <<EOL > $FILE_NAME 
+$(date +'%m/%d/%Y')
+Bash is powerful!
+Javascript is better!
+Doesn't matter! You use it in different situations...
+EOL
+
+cd ..
+INDEX_FILE="index.txt"
+NUMBER=$(cat $INDEX_FILE)
+echo "$(($NUMBER+1))" > $INDEX_FILE
+
+git add --all
+git commit -m "powerfull day $($NUMBER)"
+git push -u
